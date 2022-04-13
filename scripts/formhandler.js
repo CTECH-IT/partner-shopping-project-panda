d2297701c2f6d5fbe9f55675ccfa7b23e29bad40
Burberrys18 <98343136+Burberrys18@users.noreply.github.com>
2022-04-13 10:36:21 -0500

    worked on remotedatastore and formhandler js files

---
+++ scripts/formhandler.js
@@ -0,0 +1,41 @@
+(function(window) {
+    'use strict';
+
+    let App = window.App || {};
+    let $ = window.jQuery;
+
+    function FormHandler(selector) {
+        if (!selector) {
+            throw new Error('No selector provided!')
+        }
+
+        this.$formElement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find the element with selector: ' + selector);
+        }
+
+    }
+
+    FormHandler.prototype.addSubmitHandler = function (func) {
+        console.log('Setting the submit handler for the form...');
+
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            let data = {};
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            console.log(data);
+            func(data);
+
+            this.reset();
+            this.elements[0].focus();
+        });
+    }
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+
+})(window);
